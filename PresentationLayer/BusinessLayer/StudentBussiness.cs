17312682006d635e98548167c889a4653c3c58d6
332018 <72552532+332018@users.noreply.github.com>
2020-12-01 18:28:12 +0100

    Metode na slojevima

---
+++ PresentationLayer/BusinessLayer/StudentBussiness.cs
@@ -0,0 +1,38 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class StudentBussiness
+    {
+        private readonly StudentRepository studentRepository;
+        public StudentBussiness()
+        {
+            this.studentRepository = new StudentRepository();
+        }
+
+        public List<Student> GetStudents()
+        {
+           return this.studentRepository.GetStudents();
+        }
+
+        public bool InsertStudent(Student s)
+        {
+            if(this.studentRepository.InsertStudent(s) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<Student> GetStudentsWithMark(decimal averageMark)
+        {
+            return this.studentRepository.GetStudents().Where(s => s.AverageMark > averageMark).ToList();
+        }
+    }
+}
